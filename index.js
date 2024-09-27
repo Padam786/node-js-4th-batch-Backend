4ca7659795ab843aa0496ae90e59d2990655f47b
Padam7890 <padamthapamoho@gmail.com>
2024-09-27 10:40:26 +0545

    file system

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-import {fullname, hey} from "./module/sum"
 
-function fetchData(callback) {
-  setTimeout(() => {
-    callback("Data fetched!");
-  }, 2000);
+const fs = require('fs');
+
+const file ="hello";
+const data = " hey i am padam"
+
+// fs.writeFileSync(file,data)
+//async write file 
+
+// fs.writeFile(file,data, (err,data)=> {
+//    if (err) console.error(err);
+// })
+
+fs.readFile(file, {
+  encoding: 'utf8',
+
+}, callback)
+
+
+
+function callback (err,data) {
+   if (err) console.error(err);
+   console.log(data);
+
+
 }
 
-fetchData((message) => {
-  console.log(message);
-});
+
+console.log(" i am last please console log me")
